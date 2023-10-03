e96cd6fe9a9efbd5532a4b9e0801a977594b461f
Dalal Doss. <89653020+Daldossary@users.noreply.github.com>
2023-10-03 08:27:40 +0000

    Final edits

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 const questions = [
   {
-    id: 1,
+    id: 'q1',
     inquiry: "What is React?",
     answer: "React is a React is a JavaScript library for building user interfaces. It was created by Facebook in 2013 and is now one of the most popular web development libraries in the world." +
-    "Some of the features of React are: - It allows you to create reusable UI components that can be composed together. <br> - It uses a syntax called JSX that lets you write HTML-like code in JavaScript." +
-    "<br> - It updates the UI automatically when the data changes, using a technique called virtual DOM. <br> - It can be used to build single-page applications or integrate with other frameworks."
+    "Some of the features of React are: it allows you to create reusable UI components that can be composed together, uses syntax called JSX that lets you write HTML-like code in JavaScript" +
+    ", updates the UI automatically when the data changes, and using a technique called virtual DOM, also can be used to build single-page applications or integrate with other frameworks."
   }, {
-    id: 2,
+    id: 'q2',
     inquiry: "What are some React frameworks?",
-    answer: "Some React frameworks include: <br> - Redux <br> - Create React App <br> - Rebass" +
-    "<br> - React Admin <br> - Ant Design"
+    answer: "Some React frameworks are as follows:\
+     React Admin, and Ant Design. Redux is a predictable state container for JavaScript apps. It helps you write applications that behave consistently, run in different environments (client, server, and native)," +
+    " and are easy to test. Create React App is a tool built by developers at Facebook to help you build React applications. It saves you from time-consuming setup and configuration. You simply run one command and create react app sets up the tools you need to start your React project." +
+    " Rebass is a React UI component library that uses styled-components for styling. It provides a set of accessible, reusable, and composable React components that make it super easy to create websites and apps." +
+    " React Admin is a frontend framework for building backends for web apps. It provides a rich set of components for interacting with your data, including: a datagrid, pagination, and filters." +
+    " Ant Design is a React UI library that contains a set of high-quality components and demos for building rich, interactive user interfaces."
   }, {
-    id: 3,
+    id: 'q3',
     inquiry: "Why should I use React?",
     answer: "React is remarkbly flexible. React was created with a single focus: to create components for web applications. A React component can be anything in your web application like a Button, Text, Label, or Grid." +
     " Also Your team will fall in love with React when they start coding in it. Rapid development and React's small API combined creates a fantastic developer experience." +
@@ -22,18 +27,13 @@ const questions = [
   }
 ];
 
-function ShowButton() {
-  return <button>+</button>;
-}
-
 function QuestionCard({ counter, inquiry, answer }) {
+  const [open, setOpen] = useState(true);
   return (
     <div>
       <h3>{inquiry}</h3>
-      <ShowButton/>
-      <div className={counter}>
-        <p>{answer}</p>
-        </div>
+      <button onClick={() => setOpen(!open)}>{open ? '-' : '+'}</button>
+      <div className={ counter } style={open ? {display: 'block'} : {display: 'none'}}><p>{answer}</p></div>
     </div>
   );
 }
